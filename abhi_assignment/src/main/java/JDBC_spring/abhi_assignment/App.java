2dec8c17a42082197e36b3def4c79dea93e8f84a
abhijaiswal07 <152841109+abhijaiswal07@users.noreply.github.com>
2024-04-16 00:09:20 +0530

    Add files via upload

---
+++ abhi_assignment/src/main/java/JDBC_spring/abhi_assignment/App.java
@@ -0,0 +1,13 @@
+package JDBC_spring.abhi_assignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
